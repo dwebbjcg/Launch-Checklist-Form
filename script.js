c14e5f88a047506e80692aa3ab7134cbefeb7c7e
dwebbjcg <63323184+dwebbjcg@users.noreply.github.com>
2020-05-17 15:46:14 -0400

    Completed assn

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ window.addEventListener("load", function() {
    let cargo = document.querySelector("input[name=cargoMass]");
    let launchCheck = document.getElementById("launchStatusCheck");
    let launchStatus = document.getElementById("launchStatus");
-   let itemsList = document.getElementById("faultyItems");
    let pilotStatus = document.getElementById("pilotStatus");
    let copilotStatus = document.getElementById("copilotStatus");
    let fuelStatus = document.getElementById("fuelStatus");
@@ -16,15 +15,14 @@ window.addEventListener("load", function() {
    let destination = document.getElementById("missionTarget");
 
    form.addEventListener("submit", function() {
+      event.preventDefault();
       if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
          alert("All fields are required!");
-         event.preventDefault();
-      };
+      }
 
       if (!isNaN(pilot.value) || !isNaN(copilot.value) || isNaN(fuel.value) || isNaN(cargo.value)) {
          alert("Invalid input! Please enter valid input.");
-         event.preventDefault();
-      };
+      }
 
       if (isNaN(pilot.value) && isNaN(copilot.value) && !isNaN(fuel.value) && !isNaN(cargo.value)) {
          pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch!`;
@@ -34,17 +32,21 @@ window.addEventListener("load", function() {
             faultyItems.style.visibility = "visible";
             launchStatus.style.color = "red";
             launchStatus.innerHTML = "Shuttle not ready for launch!";
-            fuelStatus.innerHTML = "Not enough fuel to complete your mission. Please add more.";
+            fuelStatus.innerHTML = "Not enough fuel to complete your mission. Please add fuel.";
+         } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch!";
          }
 
-         else if (cargo.value > 10000) {
+         if (cargo.value > 10000) {
             faultyItems.style.visibility = "visible";
             launchStatus.style.color = "red";
             launchStatus.innerHTML = "Shuttle not ready for launch!";
             cargoStatus.innerHTML = "Too much mass to lift off. Please use the jettison method.";
+         } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch!";
          }
 
-         else {
+         if (fuel.value >= 10000 && cargo.value <= 10000) {
             faultyItems.style.visibility = "visible";
             launchStatus.style.color = "green";
             launchStatus.innerHTML = "Shuttle ready for launch!!";
@@ -52,23 +54,22 @@ window.addEventListener("load", function() {
 
          fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
             response.json().then(function(json) {
-               // console.log(json);
+               let obj_keys = Object.keys(json);
+               let rand_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
 
                destination.innerHTML = `
                <h2>Mission Destination</h2>
                <ol>
-                  <li>Name: ${json[0].name}</li>
-                  <li>Diameter: ${json[0].diameter}</li>
-                  <li>Star: ${json[0].star}</li>
-                  <li>Distance from Earth: ${json[0].distance}</li>
-                  <li>Number of Moons: ${json[0].moons}</li>
+                  <li>Name: ${json[rand_key].name}</li>
+                  <li>Diameter: ${json[rand_key].diameter}</li>
+                  <li>Star: ${json[rand_key].star}</li>
+                  <li>Distance from Earth: ${json[rand_key].distance}</li>
+                  <li>Number of Moons: ${json[rand_key].moons}</li>
                </ol>
-               <img src="${json[0].image}">`;
+               <img src="${json[rand_key].image}">`;
             });
          });
-
-         event.preventDefault();
-      };
+      }
    });
 });
 
